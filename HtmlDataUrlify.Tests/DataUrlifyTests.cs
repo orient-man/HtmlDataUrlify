962f600a0c3d1d19957c27431d2f00d8ed06ba4f
malin <malin@piatka.pl>
2014-03-12 15:16:11 +0100

    Converts images to data-uris

--- HtmlDataUrlify.Tests/DataUrlifyTests.cs
+++ HtmlDataUrlify.Tests/DataUrlifyTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
+using System.Linq;
 using FluentAssertions;
+using HtmlAgilityPack;
 using NUnit.Framework;
 
 namespace HtmlDataUrlify.Tests
@@ -10,6 +12,7 @@ namespace HtmlDataUrlify.Tests
         private const string BasePath = @"..\..\..\HtmlDataUrlify.Tests\";
 
         public const string TestPageNoImages = BasePath + "TestPageNoImages.html";
+        public const string TestPage = BasePath + "TestPage.html";
 
         private readonly DataUrlify tool = new DataUrlify();
 
@@ -20,5 +23,25 @@ namespace HtmlDataUrlify.Tests
                 .Should()
                 .Be(File.ReadAllText(TestPageNoImages));
         }
+
+        [Test]
+        public void ConvertsImgSourcesToDataUri()
+        {
+            // act
+            var html = tool.Urlify(TestPage);
+
+            // assert
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            doc.DocumentNode
+                .SelectNodes("//img")
+                .SelectMany(o => o.Attributes)
+                .Where(o => o.Name == "src")
+                .Select(o => o.Value)
+                .First()
+                .Should()
+                .StartWith("data:image/jpg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/");
+        }
+
     }
 }
--- HtmlDataUrlify/DataUrlify.cs
+++ HtmlDataUrlify/DataUrlify.cs
@@ -1,20 +1,42 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using HtmlAgilityPack;
 
 namespace HtmlDataUrlify
 {
     public class DataUrlify
     {
-        public string Urlify(string path)
+        public string Urlify(string filePath)
         {
             var doc = new HtmlDocument();
-            var html = File.ReadAllText(path);
+            var html = File.ReadAllText(filePath);
             doc.LoadHtml(html);
             var imgTags = doc.DocumentNode.SelectNodes("//img");
             if (imgTags == null || imgTags.Count == 0)
                 return html;
 
+            var rootPath = Path.GetDirectoryName(filePath) ?? "./";
+            foreach (var tag in imgTags)
+            {
+                var src = tag.Attributes.FirstOrDefault(o => o.Name == "src");
+                if (src != null)
+                {
+                    tag.SetAttributeValue(
+                        "src",
+                        GetDataUrl(Path.Combine(rootPath, src.Value)));
+                }
+            }
             return doc.DocumentNode.InnerHtml;
+        }
+
+        private static string GetDataUrl(string imgFile)
+        {
+            return
+                "data:image/"
+                + (Path.GetExtension(imgFile) ?? "").Replace(".", "")
+                + ";base64,"
+                + Convert.ToBase64String(File.ReadAllBytes(imgFile));
         }
     }
 }

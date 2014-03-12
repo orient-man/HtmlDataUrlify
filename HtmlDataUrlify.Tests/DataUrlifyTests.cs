01c9b81e1cad00a5f4a5ec23444717910b5911c9
malin <malin@piatka.pl>
2014-03-12 14:48:05 +0100

    First step: doesn't change input html :)

---
+++ HtmlDataUrlify.Tests/DataUrlifyTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace HtmlDataUrlify.Tests
+{
+    [TestFixture]
+    public class DataUrlifyTests
+    {
+        private const string BasePath = @"..\..\..\HtmlDataUrlify.Tests\";
+
+        public const string TestPageNoImages = BasePath + "TestPageNoImages.html";
+
+        private readonly DataUrlify tool = new DataUrlify();
+
+        [Test]
+        public void NoChangesWhenNoImages()
+        {
+            tool.Urlify(TestPageNoImages)
+                .Should()
+                .Be(File.ReadAllText(TestPageNoImages));
+        }
+    }
+}
---
+++ HtmlDataUrlify/DataUrlify.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace HtmlDataUrlify
+{
+    public class DataUrlify
+    {
+        public string Urlify(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}

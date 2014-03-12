13154d084e20656483cc357397a54ac2d2b6bc74
malin <malin@piatka.pl>
2014-03-12 15:17:16 +0100

    Run Urlify from Main

--- HtmlDataUrlify/Program.cs
+++ HtmlDataUrlify/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace HtmlDataUrlify
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine(new DataUrlify().Urlify(args[0]));
         }
     }
 }
